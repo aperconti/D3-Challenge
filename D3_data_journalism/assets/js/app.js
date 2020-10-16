b2a4332f5653fa510836e8aec93cd70e1bdff361
Autum Perconti <autumlucille@gmail.com>
2020-10-15 21:59:56 -0700

    adding labels to scatter plot

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,97 +1,109 @@
-// reading in csv and printing it to console to veiw data
-// d3.csv("./assets/data/data.csv", function (data) {
-//     console.log(data);
-//     // });
-
-// Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 560;
-
-// Define the chart's margins as an object
-var chartMargin = {
-    top: 30,
-    right: 30,
-    bottom: 30,
-    left: 30
-}
-
-
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-
-
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3
-    .select("#scatter")
-    .append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth);
-
-
-// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
-// to the margins set in the "chartMargin" object.
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
-
-// Load data from hours-of-tv-watched.csv
 d3.csv("./assets/data/data.csv").then(Data => {
+    // Define SVG area dimensions
+    var svgWidth = 960;
+    var svgHeight = 560;
 
-    // Print the tvData
-    console.log(Data);
+    // Define the chart's margins as an object
+    var chartMargin = {
+        top: 30,
+        right: 30,
+        bottom: 50,
+        left: 50
+    }
 
-    // Cast the hours value to a number for each piece of tvData
+    // Define dimensions of the chart area
+    var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+    var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+    // Select body, append SVG area to it, and set the dimensions
+    var svg = d3
+        .select("#scatter")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    // Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+    // to the margins set in the "chartMargin" object.
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+
+    // Cast the hours value to a number for each piece of Data
     Data.forEach(function (data) {
-        data.poverty = +data.poverty;
-        console.log(data.poverty)
+        data.smokes = +data.smokes;
+        data.age = +data.age;
     });
 
-    // Cast the hours value to a number for each piece of income
-    Data.forEach(function (data) {
-        data.income = +data.income;
-        console.log(data.income)
+    // Add some padding in the graph
+    var ageRange = d3.extent(Data, d => d.age)
 
-    });
+    ageRange[0] = ageRange[0] - 1
+    ageRange[1] = ageRange[1] + 1
 
-    var x = d3.scaleTime().range([0, svgHeight]);
-    var y = d3.scaleLinear().range([svgHeight, 0]);
+    // Create scale functions
+    var xScale = d3.scaleLinear()
+        .domain(ageRange)
+        .range([0, chartWidth]);
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(Data, d => d.smokes)])
+        .range([chartHeight, 0]);
 
-    // Scale the range of the data
-    x.domain(d3.extent(Data, function (d) {
-        return d.poverty;
-    }));
-    y.domain([0, d3.max(Data, function (d) {
-        return d.income;
-    })]);
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xScale);
+    var leftAxis = d3.axisLeft(yScale);
 
-    //defining graph type
-    var valueline = d3.line()
-        .x(function (d) {
-            return x(d.poverty);
-        })
-        .y(function (d) {
-            return y(d.income);
-        });
+    // Append axes to the chart
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
 
-    var svg = d3.select("#scatter").append("svg")
-        .attr("width", svgWidth + chartMargin.left + chartMargin.right)
-        .attr("height", svgHeight + chartMargin.top + chartMargin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top + ")");
+    chartGroup.append("g")
+        .call(leftAxis);
 
-
-    var path = svg.selectAll("dot")
+    // Create circles
+    chartGroup.selectAll("Circle")
         .data(Data)
-        .enter().append("circle")
-        .attr("r", 5)
-        .attr("cx", function (d) {
-            return x(d.poverty);
-        })
-        .attr("cy", function (d) {
-            return y(d.income);
-        })
-        .attr("stroke", "#32CD32")
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.age))
+        .attr("cy", d => yScale(d.smokes))
+        .attr("r", "20")
+        // .attr("r", d => d.poverty)
+        .attr("stroke", "green")
+        .attr("fill", "darkred")
         .attr("stroke-width", 1.5)
-        .attr("fill", "#FFFFFF");
+        .attr("opacity", "0.8")
+        .append("text");
 
+    chartGroup.selectAll(null)
+        .data(Data)
+        .enter()
+        .append("text")
+        .attr("x", function (d) {
+            return xScale(d.age);
+        })
+        .attr("y", function (d) {
+            return yScale(d.smokes) + 5;
+        })
+        .text(function (d) {
+            return d.abbr;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "12px")
+        .attr("text-anchor", "middle")
+        .attr("fill", "white");
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - chartMargin.left - 4)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Smokers");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top - 2})`)
+        .attr("class", "axisText")
+        .text("Age");
 });
